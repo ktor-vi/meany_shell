c9a1022e6a4248cafb279e420ce91e4461c745ba
Ryan Andre <randre@student.s19.be>
2024-01-08 16:26:26 +0100

    clean main + exit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: randre <randre@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 13:36:17 by randre            #+#    #+#             */
-/*   Updated: 2024/01/08 16:20:47 by randre           ###   ########.fr       */
+/*   Updated: 2024/01/08 16:25:43 by randre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(void)
 {
 	char	*line;
 	char	**split_line;
+	int		exit_flag;
 	int		i;
 
 	while (1)
@@ -25,11 +26,14 @@ int	main(void)
 		if (!ft_strcmp(line, ""))
 		{
 			split_line = ft_split(line, ' ');
-			if (ft_strcmp(split_line[0], "exit")
+			if (ft_strcmp(split_line[0], "exit"))
+				exit_flag = 1;
 			check_command(split_line);
 			while(split_line[++i])
 				free(split_line[i]);
 			free(split_line);
+			if (exit_flag == 1)
+				exit(0);
 		}
 		free(line);
 	}
