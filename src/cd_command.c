fa89891470d44f5f03de2668759e29aef8964c87
Ryan Andre <randre@student.s19.be>
2024-01-18 15:50:38 +0100

    cd corr

--- src/cd_command.c
+++ src/cd_command.c
@@ -6,7 +6,7 @@
 /*   By: randre <randre@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 10:40:05 by randre            #+#    #+#             */
-/*   Updated: 2024/01/18 11:00:56 by randre           ###   ########.fr       */
+/*   Updated: 2024/01/18 15:49:10 by randre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 void	cd_command(char **split_line)
 {
-	if (chdir(split_line[1]) != 0)
+	char *path;
+
+	if (split_line[1])
 	{
-		write(2, "cd: no such file or directory: ", 32);	
-		write(2, split_line[1], ft_strlen(split_line[1]));
-		write(2, "\n", 1);
+		if (chdir(split_line[1]) != 0)
+		{
+			write(2, "cd: no such file or directory: ", 32);	
+			write(2, split_line[1], ft_strlen(split_line[1]));
+			write(2, "\n", 1);
+		}
+	}
+	else
+	{
+		path = ft_strjoin("/Users/", getenv("USER"));
+		chdir(path);
 	}
 }
