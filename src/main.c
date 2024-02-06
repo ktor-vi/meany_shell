aed0d7d7d0d200cb6b9be3d0d4c1f8e37cf20a1c
Victor Philippe <vphilipp@fu-c3-p1.s19.be>
2024-02-06 13:10:27 +0100

    fixed prompt bug, proto piping ok

--- src/main.c
+++ src/main.c
@@ -11,73 +11,69 @@
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-void	handle_SIGINT(int sig)
-{
-	if (sig == SIGINT)
-	{
-		printf("\n");
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-	}
+
+void handle_SIGINT(int sig) {
+    if (sig == SIGINT) {
+        printf("\n");
+        rl_on_new_line();
+        rl_replace_line("", 0);
+        rl_redisplay();
+    }
 }
 
-void	handle_SIGQUIT(int sig)
-{
-	if (sig == SIGQUIT)
-		return ;
+void handle_SIGQUIT(int sig) {
+    if (sig == SIGQUIT)
+        return;
 }
 
-int	main(int argc, char **argv, char **envp)
-{
-	char		**split_line;
-	char		*line;
-	int			i;
-	int			j;
-	t_envs		*envs;
-	t_minishell	*minishell;
+int main(int argc, char **argv, char **envp) {
+    char **split_line;
+    char *line;
+    t_envs *envs;
+    t_minishell *minishell;
+		int i = 0;
+    (void)argc;
+    (void)argv;
 
-	(void)argc;
-	(void)argv;
-	envs = build_envs(envp);
-	line = NULL;
-	signal(SIGINT, handle_SIGINT);
-	signal(SIGQUIT, handle_SIGQUIT);
-	while (true)
-	{
-			write(1, "n\n", 2);
-		i = -1;
-		j = -1;
-		line = readline("$ ");
-		if(!line)
-			write(1, "k\n", 2);
-		if (line && !ft_equalstr(line, ""))
-		{
-			split_line = ft_split(line, ' ');
-			add_history(line);
-			minishell = populate(split_line, envs);
-			if (!handle_builtins(minishell->cmd, envs))
-				execute_pipes(minishell, envs->env);
-			while (split_line[++i])
-				free(split_line[i]);
-			free(split_line);
-			// while (minishell->cmd->args[++j])
-			// 	free(minishell->cmd->args[j]);
-			// free(minishell->cmd->args);
-		}
-		if(line)
-		{
-			write(1, "o\n", 2);
-			free(line);
-			line = (char *)NULL;
-		}
-		else
-		{
-			// write(1, "y\n", 2);
-			rl_clear_history();
-			exit(0);
-		}
-	}
-	return (0);
+    envs = build_envs(envp);
+    line = NULL;
+
+    signal(SIGINT, handle_SIGINT);
+    signal(SIGQUIT, handle_SIGQUIT);
+
+    while (true) {
+        line = readline("$ ");
+        
+        if (!line) {
+            write(1, "k\n", 2);
+        } else if (!ft_equalstr(line, "")) {
+            split_line = ft_split(line, ' ');
+            add_history(line);
+
+            minishell = populate(split_line, envs);
+            minishell->st_in = dup(STDIN_FILENO);
+            if (!handle_builtins(minishell->cmd, envs)) {
+                execute_pipes(minishell, envs->env);
+                dup2(minishell->st_in, STDIN_FILENO);
+            }
+            // Free allocated memory
+            while (split_line[i++]) {
+                free(split_line[i]);
+            }
+            free(split_line);
+        }
+        if (line) {
+            free(line);
+            line = NULL;
+        } else {
+            rl_clear_history();
+            exit(0);
+        }
+    }
+
+    return 0;
 }
--- src/piping.c
+++ src/piping.c
@@ -11,73 +11,62 @@
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-void execute_pipes(t_minishell *minishell, t_entry *envp)
+void	execute_pipes(t_minishell *minishell, t_entry *envp)
 {
-    t_command *h = minishell->cmd;
-    int prev_pipe = STDIN_FILENO;
-    int pfds[2];
+	t_command	*h;
+	int			prev_pipe;
+	int			pfds[2];
+	pid_t		child_pid;
+	pid_t		last_child_pid;
 
-    while (h->to_pipe == true)
-    {
-        pipe(pfds);
-        pid_t child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        }
-
-        if (child_pid == 0)
-        {
-            // Child process
-            if (prev_pipe != STDIN_FILENO)
-            {
-                dup2(prev_pipe, STDIN_FILENO);
-                close(prev_pipe);
-            }
-
-            dup2(pfds[1], STDOUT_FILENO);
-            close(pfds[0]);
-            close(pfds[1]);
-
-            execve(h->path, h->args, ll_to_tab(envp));
-            perror("execve failed");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            // Parent process
-            close(prev_pipe);
-            close(pfds[1]);
-            prev_pipe = pfds[0];
-            h = h->next;
-        }
-    }
-
-    // Parent process: close the last pipe
-    close(prev_pipe);
-    // Wait for all child processes to finish
-    while (waitpid(-1, NULL, 0) != -1)
-    {
-        // Handle child process termination status if needed
-    }
-
-    // Execute the last command after the loop
-    if (prev_pipe != STDIN_FILENO)
-    {
-        dup2(prev_pipe, STDIN_FILENO);
-        close(prev_pipe);
-    }
-
-    execve(h->path, h->args, ll_to_tab(envp));
-    perror("execve failed");
-    exit(EXIT_FAILURE);
+	h = minishell->cmd;
+	prev_pipe = STDIN_FILENO;
+	while (h->to_pipe == true)
+	{
+		if (pipe(pfds) == -1)
+			exit(EXIT_FAILURE);
+		child_pid = fork();
+		if (child_pid == -1)
+			exit(EXIT_FAILURE);
+		if (child_pid == 0)
+		{
+			close(pfds[0]); // Close the unused read end of the pipe
+			if (prev_pipe != STDIN_FILENO && dup2(prev_pipe, STDIN_FILENO) ==
+				-1)
+				exit(EXIT_FAILURE);
+			if (dup2(pfds[1], STDOUT_FILENO) == -1)
+				exit(EXIT_FAILURE);
+			close(pfds[1]);
+			execve(h->path, h->args, ll_to_tab(envp));
+			perror("execve failed");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			close(prev_pipe); // Close only the read end of the previous pipe
+			close(pfds[1]);
+			prev_pipe = pfds[0];
+			h = h->next;
+		}
+	}
+	last_child_pid = fork();
+	if (last_child_pid == -1)
+		exit(EXIT_FAILURE);
+	if (last_child_pid == 0)
+	{
+		if (prev_pipe != STDIN_FILENO && dup2(prev_pipe, STDIN_FILENO) == -1)
+			exit(EXIT_FAILURE);
+		execve(h->path, h->args, ll_to_tab(envp));
+		perror("execve failed");
+		exit(EXIT_FAILURE);
+	}
+	else
+		close(prev_pipe);
+	waitpid(last_child_pid, NULL, 0);
 }
