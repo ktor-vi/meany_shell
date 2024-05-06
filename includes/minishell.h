a6cd21563ca499f12fa5fe1c268d0b3c6a94dc88
Victor Philippe <vphilipp@fu-c3-p1.s19.be>
2024-05-06 17:00:54 +0200

    corrected header

--- includes/minishell.h
+++ includes/minishell.h
@@ -85,7 +85,7 @@ typedef struct s_minishell
 int						g_exit_codes[2];
 
 // PARSING
-char					**lexer(char *line, t_envs *envs);
+char					**lexer(char *line);
 void					print_all_cmd(t_minishell *minishell);
 t_command				*lastcmd(t_command *lst);
 void					init_cmds(char *split_line);
@@ -119,7 +119,7 @@ void					export_cmd(t_envs *envs, char *var);
 void					unset_cmd(t_envs *envs, char *var);
 t_entry					*find_entryprev(t_entry *lst, char *to_find);
 // ENV UTILS
-char					*ft_expand(char c, int *i, char *line, t_envs *envs);
+char					*ft_expand(char c, int *i, char *line);
 t_entry					*swap(t_entry *ptr1, t_entry *ptr2);
 void					sort_alpha_ll(t_entry **head, int count);
 t_entry					*lastentry(t_entry *lst);
